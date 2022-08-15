9e4d0ede4b49d957dd67ffe03f1f34f53dd102a4
Yuko Taniguchi <yuko.taniguchi@oxideinteractive.onmicrosoft.com>
2022-08-15 16:36:02 +1000

    post added

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,56 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import sanityClient from "../client.js";
+
 const Post = () => {
-  return <h1>Post Page</h1>;
+  const [postData, setPost] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type=="post"]{
+      title,
+      slug,
+      mainImage{
+        asset->{
+          _id,
+          url
+        },
+        alt
+      }
+    }`
+      )
+      .then((data) => setPost(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main>
+      <section>
+        <h1>Blog Posts Page</h1>
+        <h2>Welcome to my page of blog posts</h2>
+        <div>
+          {postData &&
+            postData.map((post, index) => (
+              <article>
+                <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                  <span key={index}>
+                    <img
+                      src={post.mainImage.asset.url}
+                      alt={post.mainImage.alt}
+                      className=""
+                    />
+                    <span className="">
+                      <h3 className="">{post.title}</h3>
+                    </span>
+                  </span>
+                </Link>
+              </article>
+            ))}
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Post;
